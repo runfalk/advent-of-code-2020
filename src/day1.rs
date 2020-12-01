aa492fee2e99baea91aecc37d0de4af65bcbe3af
Andreas Runfalk <andreas@runfalk.se>
2020-12-01 11:28:06 +0100

    Replaced solution for day 1 with a more generic and slower one

--- src/day1.rs
+++ src/day1.rs
@@ -1,25 +1,19 @@
 use anyhow::{anyhow, Result};
-use itertools::iproduct;
+use itertools::Itertools;
 use std::path::Path;
 
 use crate::reader::read_lines;
 
-fn part_a(entries: &[usize]) -> Result<usize> {
-    for (x, y) in iproduct!(entries.iter(), entries.iter()) {
-        if x + y == 2020 {
-            return Ok(x * y);
+/// Find num_entries in entries that sum to 2020 and return their product
+fn find_product_2020(entries: &[usize], num_entries: usize) -> Result<usize> {
+    for selection in entries.iter().combinations(num_entries) {
+        // Check if the selection sums up to 2020
+        if selection.iter().cloned().sum::<usize>() == 2020 {
+            // Return the product of the selection
+            return Ok(selection.into_iter().fold(1, |p, f| p * f));
         }
     }
-    Err(anyhow!("No matching transactions"))
-}
-
-fn part_b(entries: &[usize]) -> Result<usize> {
-    for (x, y, z) in iproduct!(entries.iter(), entries.iter(), entries.iter()) {
-        if x + y + z == 2020 {
-            return Ok(x * y * z);
-        }
-    }
-    Err(anyhow!("No matching transactions"))
+    Err(anyhow!("No matching entries"))
 }
 
 pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
@@ -27,5 +21,25 @@ pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
         .into_iter()
         .map(|l| Ok(l?.parse()?))
         .collect::<Result<Vec<_>>>()?;
-    Ok((part_a(&entries)?, Some(part_b(&entries)?)))
+    Ok((
+        find_product_2020(&entries, 2)?,
+        Some(find_product_2020(&entries, 3)?),
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_invalid_input() -> Result<()> {
+        assert!(find_product_2020(&[1010], 2).is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn test_tricky_input() -> Result<()> {
+        assert_eq!(find_product_2020(&[1010, 2010, 10], 2)?, 20100);
+        Ok(())
+    }
 }
