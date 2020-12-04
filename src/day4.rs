9b0379bd66918b08097547afe5478eba46626bcb
Andreas Runfalk <andreas@runfalk.se>
2020-12-04 11:18:24 +0100

    Updated solution for day 4 to be more efficient

--- src/day4.rs
+++ src/day4.rs
@@ -1,11 +1,27 @@
 use anyhow::{anyhow, Error, Result};
 use once_cell::sync::Lazy;
 use regex::Regex;
-use std::collections::{HashMap, HashSet};
+use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::fmt;
+use std::hash::Hash;
 use std::path::Path;
 use std::str::FromStr;
 
 use crate::reader::read_lines;
+
+#[derive(Debug)]
+struct KeyError<'a, T: ?Sized> {
+    key: &'a T,
+}
+
+impl<'a, T: fmt::Display + ?Sized> fmt::Display for KeyError<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "No key {}", self.key)
+    }
+}
+
+impl<'a, T: fmt::Debug + fmt::Display + ?Sized> std::error::Error for KeyError<'a, T> {}
 
 static HEIGHT_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\d+)(cm|in)$").unwrap());
 static COLOR_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^#[a-f0-9]{6}$").unwrap());
@@ -31,50 +47,55 @@ impl FromStr for Height {
     }
 }
 
-pub fn part2(passport: &HashMap<String, String>) -> Result<()> {
-    let byr: usize = passport
-        .get("byr")
-        .ok_or(anyhow!("No byr field"))?
-        .parse()?;
-    if !(1920..=2002).contains(&byr) {
+fn get_field<'a, 'b, K, V, Q>(
+    map: &'a HashMap<K, V>,
+    key: &'b Q,
+) -> std::result::Result<&'a V, KeyError<'b, Q>>
+where
+    K: Eq + Hash + Borrow<Q>,
+    Q: Eq + Hash + fmt::Display + ?Sized,
+{
+    map.get(key).ok_or_else(|| KeyError { key })
+}
+
+pub fn validate_passport(passport: &HashMap<String, String>) -> Result<()> {
+    // We need to extract the all fields before value validation since part A
+    // only requires the field to be present
+    let byr = get_field(passport, "byr")?;
+    let iyr = get_field(passport, "iyr")?;
+    let eyr = get_field(passport, "eyr")?;
+    let hgt = get_field(passport, "hgt")?;
+    let hcl = get_field(passport, "hcl")?;
+    let ecl = get_field(passport, "ecl")?;
+    let pid = get_field(passport, "pid")?;
+
+    if !(1920..=2002).contains(&byr.parse::<usize>()?) {
         return Err(anyhow!("Invalid byr"));
     }
 
-    let iyr: usize = passport
-        .get("iyr")
-        .ok_or(anyhow!("No iyr field"))?
-        .parse()?;
-    if !(2010..=2020).contains(&iyr) {
+    if !(2010..=2020).contains(&iyr.parse::<usize>()?) {
         return Err(anyhow!("Invalid iyr"));
     }
 
-    let eyr: usize = passport
-        .get("eyr")
-        .ok_or(anyhow!("No eyr field"))?
-        .parse()?;
-    if !(2020..=2030).contains(&eyr) {
+    if !(2020..=2030).contains(&eyr.parse::<usize>()?) {
         return Err(anyhow!("Invalid eyr"));
     }
 
-    match passport
-        .get("hgt")
-        .ok_or(anyhow!("No hgt field"))?
-        .parse()?
-    {
+    match hgt.parse()? {
         Height::Cm(150..=193) => (),
         Height::In(59..=76) => (),
         _ => return Err(anyhow!("Invalid hgt")),
     };
 
-    if !COLOR_RE.is_match(passport.get("hcl").ok_or(anyhow!("No hcl field"))?) {
+    if !COLOR_RE.is_match(hcl) {
         return Err(anyhow!("Invalid hcl"));
     }
 
-    if !EYE_COLOR_RE.is_match(passport.get("ecl").ok_or(anyhow!("No ecl field"))?) {
+    if !EYE_COLOR_RE.is_match(ecl) {
         return Err(anyhow!("Invalid ecl"));
     }
 
-    if !PID_RE.is_match(passport.get("pid").ok_or(anyhow!("No pid field"))?) {
+    if !PID_RE.is_match(pid) {
         return Err(anyhow!("Invalid pid"));
     }
 
@@ -82,6 +103,7 @@ pub fn part2(passport: &HashMap<String, String>) -> Result<()> {
 }
 
 pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
+    // Read all passports in a vector of maps from field to value
     let re = Regex::new(r"([^: ]+):(\S+)")?;
     let mut passports = Vec::new();
     passports.push(HashMap::new());
@@ -97,28 +119,19 @@ pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
                 .insert(c[1].to_owned(), c[2].to_owned());
         }
     }
-    passports.push(HashMap::new());
 
     let mut num_valid_a = 0;
     let mut num_valid_b = 0;
-    let required_fields: HashSet<_> = vec![
-        "byr".to_owned(),
-        "iyr".to_owned(),
-        "eyr".to_owned(),
-        "hgt".to_owned(),
-        "hcl".to_owned(),
-        "ecl".to_owned(),
-        "pid".to_owned(),
-    ]
-    .into_iter()
-    .collect();
     for p in passports {
-        let fields: HashSet<_> = p.keys().cloned().collect();
-        if required_fields.difference(&fields).count() == 0 {
-            num_valid_a += 1;
-            if part2(&p).is_ok() {
+        match validate_passport(&p) {
+            Ok(_) => {
+                num_valid_a += 1;
                 num_valid_b += 1;
             }
+            Err(e) if !e.is::<KeyError<'_, str>>() => {
+                num_valid_a += 1;
+            }
+            Err(_) => {}
         }
     }
 
