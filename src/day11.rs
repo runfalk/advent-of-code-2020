18596cad9a6f6f67687ea7e32abb9e03aa690563
Andreas Runfalk <andreas@runfalk.se>
2020-12-13 01:28:16 +0100

    Refactored day 11 solution to be more compact

--- src/day11.rs
+++ src/day11.rs
@@ -1,7 +1,6 @@
 use anyhow::{anyhow, Result};
 use std::path::Path;
 
-// use crate::reader::Grid;
 use crate::coord::Coord;
 use crate::reader::read_lines;
 
@@ -44,17 +43,21 @@ impl Tile {
     }
 }
 
-fn part_a(mut layout: Layout) -> Result<usize> {
+fn run_until_steady_state<F>(
+    mut layout: Layout,
+    max_neighbors: usize,
+    num_neighbors: F,
+) -> Result<usize>
+where
+    F: Fn(&Layout, &Coord) -> usize,
+{
     let mut new_tiles = Vec::with_capacity(layout.tiles.len());
     loop {
         for y in 0..layout.height {
             for x in 0..layout.width {
                 let c = Coord::new(x as isize, y as isize);
                 let tile = layout.get(&c).ok_or(anyhow!("Error"))?;
-                let num_neighbors = c
-                    .iter_all_neighbors()
-                    .filter(|n| layout.get(&n) == Some(Tile::Occupied))
-                    .count();
+                let num_neighbors = num_neighbors(&layout, &c);
                 new_tiles.push(match tile {
                     Tile::Floor => Tile::Floor,
                     Tile::Empty => {
@@ -65,7 +68,7 @@ fn part_a(mut layout: Layout) -> Result<usize> {
                         }
                     }
                     Tile::Occupied => {
-                        if num_neighbors >= 4 {
+                        if num_neighbors >= max_neighbors {
                             Tile::Empty
                         } else {
                             Tile::Occupied
@@ -87,61 +90,36 @@ fn part_a(mut layout: Layout) -> Result<usize> {
         .count())
 }
 
-fn part_b(mut layout: Layout) -> Result<usize> {
-    let mut new_tiles = Vec::with_capacity(layout.tiles.len());
-    loop {
-        for y in 0..layout.height {
-            for x in 0..layout.width {
-                let c = Coord::new(x as isize, y as isize);
-                let tile = layout.get(&c).ok_or(anyhow!("Error"))?;
-                let num_neighbors = Coord::origin()
-                    .iter_all_neighbors()
-                    .filter(|d| {
-                        for i in 1.. {
-                            let ray = Coord::new(c.x + d.x * i, c.y + d.y * i);
-                            match layout.get(&ray) {
-                                Some(Tile::Floor) => {}
-                                Some(Tile::Occupied) => {
-                                    return true;
-                                }
-                                Some(Tile::Empty) | None => {
-                                    return false;
-                                }
-                            };
+fn part_a(layout: Layout) -> Result<usize> {
+    run_until_steady_state(layout, 4, |layout, coord| {
+        coord
+            .iter_all_neighbors()
+            .filter(|n| layout.get(&n) == Some(Tile::Occupied))
+            .count()
+    })
+}
+
+fn part_b(layout: Layout) -> Result<usize> {
+    run_until_steady_state(layout, 5, |layout, coord| {
+        Coord::origin()
+            .iter_all_neighbors()
+            .filter(|d| {
+                for i in 1.. {
+                    let ray = Coord::new(coord.x + d.x * i, coord.y + d.y * i);
+                    match layout.get(&ray) {
+                        Some(Tile::Floor) => {}
+                        Some(Tile::Occupied) => {
+                            return true;
                         }
-                        unreachable!();
-                    })
-                    .count();
-                new_tiles.push(match tile {
-                    Tile::Floor => Tile::Floor,
-                    Tile::Empty => {
-                        if num_neighbors == 0 {
-                            Tile::Occupied
-                        } else {
-                            Tile::Empty
+                        Some(Tile::Empty) | None => {
+                            return false;
                         }
-                    }
-                    Tile::Occupied => {
-                        if num_neighbors >= 5 {
-                            Tile::Empty
-                        } else {
-                            Tile::Occupied
-                        }
-                    }
-                });
-            }
-        }
-        if new_tiles == layout.tiles {
-            break;
-        }
-        std::mem::swap(&mut layout.tiles, &mut new_tiles);
-        new_tiles.clear();
-    }
-    Ok(layout
-        .tiles
-        .into_iter()
-        .filter(|t| *t == Tile::Occupied)
-        .count())
+                    };
+                }
+                unreachable!();
+            })
+            .count()
+    })
 }
 
 pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
@@ -166,7 +144,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_a() -> Result<()> {
+    fn test_example() -> Result<()> {
         let layout = Layout {
             width: 10,
             height: 10,
@@ -187,32 +165,7 @@ mod tests {
             .flatten()
             .collect(),
         };
-        assert_eq!(part_a(layout)?, 37);
-        Ok(())
-    }
-
-    #[test]
-    fn test_part_b() -> Result<()> {
-        let layout = Layout {
-            width: 10,
-            height: 10,
-            tiles: vec![
-                "L.LL.LL.LL",
-                "LLLLLLL.LL",
-                "L.L.L..L..",
-                "LLLL.LL.LL",
-                "L.LL.LL.LL",
-                "L.LLLLL.LL",
-                "..L.L.....",
-                "LLLLLLLLLL",
-                "L.LLLLLL.L",
-                "L.LLLLL.LL",
-            ]
-            .into_iter()
-            .map(|l| l.chars().map(|c| Tile::from_char(c).unwrap()))
-            .flatten()
-            .collect(),
-        };
+        assert_eq!(part_a(layout.clone())?, 37);
         assert_eq!(part_b(layout)?, 26);
         Ok(())
     }
